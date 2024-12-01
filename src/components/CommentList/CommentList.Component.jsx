525cb963289ad512a9416e29f67715a01a24c2b9
francesca1991-dot <francescalai91@yahoo.com>
2024-12-01 14:53:40 +0100

    aggiunto modifiche

--- src/components/CommentList/CommentList.Component.jsx
+++ src/components/CommentList/CommentList.Component.jsx
@@ -66,7 +66,7 @@ function CommentList({ commentObj, deleteComment, updateComment }) {
             className="me-2 ms-1 custom-button"
             onClick={() => deleteComment(_id)}
           >
-           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
 <linearGradient id="hbE9Evnj3wAjjA2RX0We2a_OZuepOQd0omj_gr1" x1="7.534" x2="27.557" y1="7.534" y2="27.557" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#f44f5a"></stop><stop offset=".443" stop-color="#ee3d4a"></stop><stop offset="1" stop-color="#e52030"></stop></linearGradient><path fill="url(#hbE9Evnj3wAjjA2RX0We2a_OZuepOQd0omj_gr1)" d="M42.42,12.401c0.774-0.774,0.774-2.028,0-2.802L38.401,5.58c-0.774-0.774-2.028-0.774-2.802,0	L24,17.179L12.401,5.58c-0.774-0.774-2.028-0.774-2.802,0L5.58,9.599c-0.774,0.774-0.774,2.028,0,2.802L17.179,24L5.58,35.599	c-0.774,0.774-0.774,2.028,0,2.802l4.019,4.019c0.774,0.774,2.028,0.774,2.802,0L42.42,12.401z"></path><linearGradient id="hbE9Evnj3wAjjA2RX0We2b_OZuepOQd0omj_gr2" x1="27.373" x2="40.507" y1="27.373" y2="40.507" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#a8142e"></stop><stop offset=".179" stop-color="#ba1632"></stop><stop offset=".243" stop-color="#c21734"></stop></linearGradient><path fill="url(#hbE9Evnj3wAjjA2RX0We2b_OZuepOQd0omj_gr2)" d="M24,30.821L35.599,42.42c0.774,0.774,2.028,0.774,2.802,0l4.019-4.019	c0.774-0.774,0.774-2.028,0-2.802L30.821,24L24,30.821z"></path>
 </svg>
           </Button>
@@ -76,7 +76,7 @@ function CommentList({ commentObj, deleteComment, updateComment }) {
             className="custom-button"
             onClick={handleEdit}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
 <linearGradient id="iFo2NHV0hSUm8p4rOmjjAa_1NNIMsM95n2A_gr1" x1="18.9" x2="29.609" y1="-1.589" y2="59.145" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e8e189"></stop><stop offset="1" stop-color="#8f4433"></stop></linearGradient><polygon fill="url(#iFo2NHV0hSUm8p4rOmjjAa_1NNIMsM95n2A_gr1)" points="30.485,9.029 8.029,31.485 16.515,39.971 38.971,17.515"></polygon><linearGradient id="iFo2NHV0hSUm8p4rOmjjAb_1NNIMsM95n2A_gr2" x1="33.447" x2="39.376" y1="-12.227" y2="21.399" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffe2a6"></stop><stop offset="1" stop-color="#903a51"></stop></linearGradient><path fill="url(#iFo2NHV0hSUm8p4rOmjjAb_1NNIMsM95n2A_gr2)" d="M33.743,5.772l-3.257,3.257l8.485,8.485l3.257-3.257c2.343-2.343,2.343-6.142,0-8.485l0,0	C39.885,3.429,36.086,3.429,33.743,5.772z"></path><linearGradient id="iFo2NHV0hSUm8p4rOmjjAc_1NNIMsM95n2A_gr3" x1="3.164" x2="12.217" y1="24.371" y2="49.243" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e8e189" stop-opacity="0"></stop><stop offset="1" stop-color="#8f4433"></stop></linearGradient><polygon fill="url(#iFo2NHV0hSUm8p4rOmjjAc_1NNIMsM95n2A_gr3)" points="8.029,31.485 3,45 16.515,39.971"></polygon>
 </svg>
           </Button>
@@ -86,7 +86,8 @@ function CommentList({ commentObj, deleteComment, updateComment }) {
             onClick={handleSave}
             disabled={!isEditing}
           >
-             <img src={iconSave} alt="Save Icon" className="img-fluid" />
+             <img src={iconSave} alt="Save Icon" className="img-fluid" style={{ width: "30px" }} />
+
           </Button>
         </div>
       </Card.Body>
