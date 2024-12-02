aeb5d3e485259c64701afd0c2a82a1926f4611ac
francesca1991-dot <francescalai91@yahoo.com>
2024-12-02 19:45:58 +0100

    aggiunto modifiche

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router";
 
 test('renders learn react link', () => {
   render(<App />);
