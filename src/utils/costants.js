517b9c32cdae7a4453e8e8c4ac34a9613d3a6fb2
francesca1991-dot <francescalai91@yahoo.com>
2024-12-02 19:45:35 +0100

    aggiunto modifiche

--- src/utils/costants.js
+++ src/utils/costants.js
@@ -10,10 +10,3 @@ export {GET_COMMENT_URL, POST_COMMENT_URL, PUT_COMMENT_URL, API_KEY};
 
 
 
-
-/*fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
-    headers: {
- "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzE3YWFhNjVkNzU4NzAwMTUzNzg2MzYiLCJpYXQiOjE3MzE5NDYyMDIsImV4cCI6MTczMzE1NTgwMn0.eDiMU9RjTStbqpgm3N4TPaX9QQVeOSvzPvQ3jqgwE6g"
-   }
-   })
-   */
